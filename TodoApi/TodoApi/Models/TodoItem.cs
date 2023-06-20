be0071ad80b72111a4185e57f6c443df8bd815fc
Anna <annaocirova5@gmail.com>
2023-06-20 13:04:35 +0800

    создана модель DTO

--- TodoApi/TodoApi/Models/TodoItem.cs
+++ TodoApi/TodoApi/Models/TodoItem.cs
@@ -1,5 +1,10 @@
 ﻿namespace TodoApi.Models;
-
+public class TodoItemDTO
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
 public class TodoItem
 {
     public long Id { get; set; }
